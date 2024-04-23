cb2b87cbfe9303a67e78f69164aae61a701a2cbf
ComputerElite <71177995+ComputerElite@users.noreply.github.com>
2024-04-23 17:20:21 +0200

    add something idk

--- fursuit/src/imu.cpp
+++ fursuit/src/imu.cpp
@@ -1,4 +1,6 @@
 #include "imu.h"
+#include "main.h"
+#include <vector>
 
 DFRobot_BMI160 bmi160;
 const int8_t i2c_addr = 0x69;
@@ -8,7 +10,9 @@ int rslt;
 bool inAir = false; // smoothed value wether IMU thinks it's in the air
 bool inAirRaw = false; // raw value wether IMU thinks it's in the air
 int timesNotInAir = 0; // times the IMU thinks it's not in the air consecutively
+long timeOfFirstNotInAirMeasurement = 0; // time of the first not in air measurement in ms
 int timesInAir = 0; // times the IMU thinks it's in the air consecutively
+long timeOfFirstInAirMeasurement = 0; // time of the first in air measurement in ms
 bool inAirStartSignal = false; // signal that we've just started being in the air this update
 long inAirStartSignalTime = 0; // time we've just started being in the in ms
 
@@ -21,6 +25,17 @@ long inAirSignalTime = 0; // time we've been in the air in ms
 bool toggledInAirStartSignal = false; // whether or not we already toggled the start signal
 bool toggledInAirMiddleSignal = false; // whether or not we already toggled the middle signal
 bool toggledInAirEndSignal = false; // whether or not we already toggled the end signal
+
+bool toggledBeatSignal = false; // whether or not we already toggled the jump signal
+bool beatSignal = false; // signal that we've just jumped this update
+
+long currentJumpLength = 0; // Length from start of last jump to start of jump in ms
+std::vector<long> jumpLengths = {0};
+long avgJumpLength = 0;
+const int maxJumpLengths = 3;
+double bpm = 0;
+double bps = 0;
+bool isStrongBeat = false;
 
 double accelerationMagnitude = 0; // magnitude of the acceleration
 
@@ -46,16 +61,24 @@ bool ReadIMU() {
     return false;
   }
 }
+long correctedTime = 0;
 
 void UpdateVariables() {
   // Record whether or not we're in the air according to the measurement
   inAirRaw = accelerationMagnitude < 0.7;
+  correctedTime = millis();
 
   // Count the times the IMU thinks it's in the air.
   if(inAirRaw){
-    timesNotInAir = 0;
+    if(timesInAir == 0) {
+      timeOfFirstInAirMeasurement = correctedTime; // set time of first measurement for later use
+    }
     timesInAir++;
+    timesNotInAir = 0;
   } else {
+    if(timesNotInAir == 1) {
+      timeOfFirstNotInAirMeasurement = correctedTime; // set time of first measurement for later use
+    }
     timesNotInAir++;
     timesInAir = 0;
   }
@@ -67,8 +90,11 @@ void UpdateVariables() {
   if(timesInAir >= 2) {
     inAir = true;
     if(!toggledInAirStartSignal) {
-      inAirStartSignalTime = millis();
+      currentJumpLength = correctedTime - inAirStartSignalTime;
+      jumpLengths.push_back(currentJumpLength);
+      inAirStartSignalTime = timeOfFirstInAirMeasurement;
       inAirStartSignal = true;
+      toggledBeatSignal = false;
     }
     toggledInAirStartSignal = true;
 
@@ -78,7 +104,7 @@ void UpdateVariables() {
 
 
     if(!toggledInAirEndSignal) {
-      inAirEndSignalTime = millis();
+      inAirEndSignalTime = timeOfFirstNotInAirMeasurement;
       inAirEndSignal = true;
 
       inAirSignalTime = inAirEndSignalTime - inAirStartSignalTime;
@@ -88,16 +114,43 @@ void UpdateVariables() {
     toggledInAirStartSignal = false;
   }
 
-  inAirMiddleSignal = false;
   // Trigger estimated air signals
+  inAirMiddleSignal = false;
   if(inAir) {
-    if(inAirStartSignalTime + inAirSignalTime/2 <= millis() && !toggledInAirMiddleSignal) {
+    if(inAirStartSignalTime + inAirSignalTime/2 <= correctedTime && !toggledInAirMiddleSignal) {
       inAirMiddleSignal = true;
       toggledInAirMiddleSignal = true;
     }
   } else {
     toggledInAirMiddleSignal = false;
   }
+
+  // Calculate average jump length
+  if(jumpLengths.size() > maxJumpLengths) {
+    jumpLengths.erase(jumpLengths.begin());
+  }
+  avgJumpLength = 0;
+  for(int i=0; i<jumpLengths.size(); i++) {
+    avgJumpLength += jumpLengths[i];
+  }
+  avgJumpLength /= jumpLengths.size();
+  
+  // calculate bpm based on avg jump length
+  bps = 1000.0 / static_cast<double>(avgJumpLength);
+  bpm = bps * 60.0;
+
+  // Trigger beat signal
+  beatSignal = false;
+  if(inAirStartSignalTime + inAirSignalTime / 2 + avgJumpLength / 2 <= correctedTime && !toggledBeatSignal) {
+    toggledBeatSignal = true;
+    isStrongBeat ^= true;
+    beatSignal = true;
+    Serial.print("beat ");
+    Serial.println(correctedTime);
+    Serial.print("bpm ");
+    Serial.println(bpm);
+  }
+
 }
 
 void UpdateIMU() {
